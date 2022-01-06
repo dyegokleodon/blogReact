0a31b384e3cf5177ecd95bdcc9cfaab90e82638f
dyego kleodon <dyego.kleodon@gmail.com>
2022-01-06 13:29:49 -0300

    first commit projeto blogReact

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState, useMemo, useEffect, useRef} from 'react';
+import {ThemeProvider} from 'styled-components';
 
 import GlobalStyle from './styles/global';
 import Layout from './components/Layout';
 
+import themes from './styles/themes';
+
 function App() {
+
+  
+  const localTheme = JSON.parse(localStorage.getItem('theme'))
+
+  const [theme, setTheme] = useState(localTheme);
+
+  const firstRender = useRef(true);
+ 
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme]);
+
+  function handleToogleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  }
+
+  useEffect(() => {
+    if(firstRender.current){
+      firstRender.current = false;
+      return;
+    }
+
+    console.debug({theme})
+  }, [theme])
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme])
+  
   return (
-    <>
+    <ThemeProvider theme={currentTheme} >
       <GlobalStyle />
-      <Layout />
-    </>
+      <Layout onToogleTheme={handleToogleTheme} selectedTheme={theme}/>
+    </ThemeProvider>
   );
 };
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Footer() {
+export default function Footer({onToogleTheme, selectedTheme}) {
   return (
     <Container>
       <span>JStack's Blog. Todos os direitos reservados.</span>
-      <button type="button">🌞</button>
+      <button 
+        type="button" 
+        onClick={onToogleTheme}
+      >
+        {selectedTheme === 'dark' ? '🌞' : '🌚'}   
+      </button>
     </Container>
   );
 }
